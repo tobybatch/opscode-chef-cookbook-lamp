cd314efc061117a922af135406b311ad23ad5d85
James Thompson <me@jtgraphic.net>
2014-10-07 00:05:12 -0400

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'support@daftlabs.com'
 license          'All rights reserved'
 description      'Installs/Configures a LAMP server.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.16.0'
+version          '0.17.0'
 
 depends 'apt'
 depends 'ark'
